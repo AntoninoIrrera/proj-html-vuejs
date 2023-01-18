ed51cb21a92bc5060910dddce34b5353a3463c16
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-18 15:58:38 +0100

    creazione stuttura prima section del main (SectionJumbo1)

--- src/components/ComponentsMain/SectionJumbotron1.vue
+++ src/components/ComponentsMain/SectionJumbotron1.vue
@@ -6,10 +6,30 @@
 
 <template>
     <section>
-        
+        <p class="m-0 text-white testo_centrale">prova</p>
+        <p class="m-0 text-white bottone_laterale">prova</p>
     </section>
 </template>
 
 <style lang="scss" scoped>
+
+section{
+    background-color: red;
+    height: 75vh;
+    position: relative;
+}
+
+.testo_centrale{
+    position: absolute;
+    left: 60%;
+    top: 50%;
+}
+
+.bottone_laterale{
+    position: absolute;
+    bottom: 60px;
+    right: 75px;
+}
+
 
 </style>
