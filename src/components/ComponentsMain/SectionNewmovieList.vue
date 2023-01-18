b5c251990158cfa26c20f63f2adcd2f2480b674d
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-18 16:18:53 +0100

    creazione struttura quinta section del main(SectionNewmovieList)

--- src/components/ComponentsMain/SectionNewmovieList.vue
+++ src/components/ComponentsMain/SectionNewmovieList.vue
@@ -5,9 +5,48 @@
 </script>
 
 <template>
-
+    <section>
+        <div class="container py-5 px-3">
+            <div class="row">
+                <div class="col-12">
+                    <p class="text-white m-0">prova</p>
+                </div>
+                <div class="col-10 mt-5">
+                    <p class="text-white m-0">prova</p>
+                </div>
+                <div class="col-4 mt-5">
+                    <div class="prova"></div>
+                </div>
+                <div class="col-4 mt-5">
+                    <div class="prova"></div>
+                </div>
+                <div class="col-4 mt-5">
+                    <div class="prova"></div>
+                </div>
+                <div class="col-4 mt-5">
+                    <div class="prova"></div>
+                </div>
+                <div class="col-4 mt-5">
+                    <div class="prova"></div>
+                </div>
+                <div class="col-4 mt-5">
+                    <div class="prova"></div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
+@use '../../styles/partials/variables' as *;
 
+div.col-12 {
+    border-bottom: 1px solid $color_grey;
+}
+
+.prova {
+    background-color: red;
+    width: 100%;
+    height: 250px;
+}
 </style>
