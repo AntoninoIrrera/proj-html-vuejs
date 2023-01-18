86eebb8c2240c3bf37e58ba4c436a65a7a523546
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-18 16:28:44 +0100

    creazione struttura settima section del main (SectionSubscribe)

--- src/components/ComponentsMain/SectionSubscribe.vue
+++ src/components/ComponentsMain/SectionSubscribe.vue
@@ -5,9 +5,30 @@
 </script>
 
 <template>
-
+    <section>
+        <div class="container py-5 px-3">
+            <div class="row">
+                <div class="col-4">
+                    <p class="text-white m-0">prova</p>
+                </div>
+                <div class="col-4">
+                    <p class="text-white m-0">prova</p>
+                </div>
+                <div class="col-4">
+                    <p class="text-white m-0">prova</p>
+                </div>
+                <div class="col-12">
+                    <p class="text-white text-center m-0">prova</p>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
+@use '../../styles/partials/variables' as *;
 
+div.container{
+    background-color: $color_green;
+}
 </style>
