a7ebf6be076d85ad9b4dfe13865d6e1eeb9f4fe5
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-18 14:59:26 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script >
+
+
+
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+
+@use './styles/general.scss' as *;
+@use '../node_modules/bootstrap/scss/bootstrap.scss' as *;
+
+
+</style>
