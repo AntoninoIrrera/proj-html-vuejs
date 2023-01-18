994a3b0c2ff9315049a662a9732e2f96e11cd45d
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-18 15:38:59 +0100

    creazione struttura terza section dell'header (SectionNavbar)

--- src/components/ComponentsHeader/SectionNavbar.vue
+++ src/components/ComponentsHeader/SectionNavbar.vue
@@ -1,13 +1,22 @@
-<script >
+<script>
 
 
 
 </script>
 
 <template>
-    <header>
-
-    </header>
+    <section>
+        <div class="container p-3">
+            <div class="row">
+                <div class="col-6">
+                    <p class="text-white m-0">prova</p>
+                </div>
+                <div class="col-6">
+                    <p class="text-white text-end m-0">prova</p>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
