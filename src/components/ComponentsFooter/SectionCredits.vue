a38dcdcdcf36aed9c85cd188706c3fd74ddacb64
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-18 16:36:48 +0100

    creazione stuttura seconda section del main( SectionCreadits)

--- src/components/ComponentsFooter/SectionCredits.vue
+++ src/components/ComponentsFooter/SectionCredits.vue
@@ -4,7 +4,18 @@
 </script>
 
 <template>
-
+    <section>
+        <div class="container py-5 px-3">
+            <div class="row">
+                <div class="col-6">
+                    <p class="text-white m-0">prova</p>
+                </div>
+                <div class="col-6">
+                    <p class="text-white m-0 text-end">prova</p>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
