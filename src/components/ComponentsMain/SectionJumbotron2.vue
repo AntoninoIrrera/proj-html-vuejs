260cbbfb3f45e3dd2c98415ea71d7cb3a7a01dfd
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-18 16:13:05 +0100

    Creazione struttura quarta section del main(Section jumbo2)

--- src/components/ComponentsMain/SectionJumbotron2.vue
+++ src/components/ComponentsMain/SectionJumbotron2.vue
@@ -5,9 +5,21 @@
 </script>
 
 <template>
-
+    <section>
+        <p class="m-0 text-white testo_centrale">prova</p>
+    </section>
 </template>
 
 <style lang="scss" scoped>
+    section {
+        background-color: red;
+        height: 75vh;
+        position: relative;
+    }
 
+    .testo_centrale {
+        position: absolute;
+        left: 60%;
+        top: 50%;
+    }
 </style>
