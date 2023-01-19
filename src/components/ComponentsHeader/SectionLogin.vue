37ae932ca8d5b23b69f2d01f6ef1cb901e7ce8d3
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-19 10:14:30 +0100

    inizio inserimento contenuti prima section dell'header

--- src/components/ComponentsHeader/SectionLogin.vue
+++ src/components/ComponentsHeader/SectionLogin.vue
@@ -9,10 +9,24 @@
         <div class="container p-3">
             <div class="row">
                 <div class="col-6">
-                    <p class="text-white m-0">prova</p>
+                    <p class="text-white m-0 d-inline-block">
+                        <font-awesome-icon class="vertical_allign" icon="fa-regular fa-envelope"/>
+                        Superhit Top Movie
+                        <span>**King Star**</span>
+                    </p>
+                    <p class="text-white m-0 ms-5 d-inline-block">
+                        <font-awesome-icon class="facebook ms-3" icon="fa-brands fa-facebook-f" />
+                        <font-awesome-icon class="twitter ms-3" icon="fa-brands fa-twitter" />
+                        <font-awesome-icon class="linkedin ms-3" icon="fa-brands fa-linkedin-in" />
+                        <font-awesome-icon class="instagram ms-3" icon="fa-brands fa-instagram"/>
+                    </p>
+                    <font-awesome-icon class="google ms-3" icon="fa-brands fa-google-plus-g" />
                 </div>
                 <div class="col-6">
-                    <p class="text-white text-end m-0">prova</p>
+                    <p class="text-white text-end m-0">
+                        <font-awesome-icon icon="fa-solid fa-lock" />
+                        Login
+                    </p>
                 </div>
             </div>
         </div>
@@ -20,11 +34,18 @@
 </template>
 
 <style lang="scss" scoped>
-
+@use '../../styles/general.scss' as *;
 @use '../../styles/partials/variables' as *;
+
+.vertical_allign{
+    vertical-align: middle;
+}
 
 section{
     border-bottom: 1px solid $color_grey;
 }
 
+span{
+    color: $color_green;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -7,8 +7,24 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faEnvelope)
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+
+import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons'
+
+import { faLock } from '@fortawesome/free-solid-svg-icons'
+
+
+
+
+
+
+library.add(faEnvelope, faFacebookF, faTwitter, faLinkedinIn, faInstagram, faGooglePlusG, faLock)
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
