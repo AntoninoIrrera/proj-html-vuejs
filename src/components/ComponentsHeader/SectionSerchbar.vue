0a41f57e3348c9256a32acaa2dc33ed6be59602d
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-19 10:22:44 +0100

    inizio inserimento contenuti seconda section dell'header

--- src/components/ComponentsHeader/SectionSerchbar.vue
+++ src/components/ComponentsHeader/SectionSerchbar.vue
@@ -9,10 +9,15 @@
         <div class="container py-5 px-3">
             <div class="row">
                 <div class="col-6">
-                    <p class="text-white m-0">prova</p>
+                    <p class="text-white m-0">
+                        <img src="../../assets/img/cropped-logo.png" alt="Logo">
+                    </p>
                 </div>
                 <div class="col-6">
-                    <p class="text-white text-end m-0">prova</p>
+                    <p class="text-white text-end m-0">
+                        <input type="text" placeholder="Search ...">
+                        <a href="#" class="bottone">Search</a>
+                    </p>
                 </div>
             </div>
         </div>
@@ -20,10 +25,29 @@
 </template>
 
 <style lang="scss" scoped>
-
-    @use '../../styles/partials/variables' as *;
+@use '../../styles/partials/variables' as *;
 
     section{
         border-bottom: 1px solid $color_grey;
+    }
+
+    input{
+        height: 53.5px;
+        width: 300px;
+        background-color: transparent;
+        border: none;
+        border-top: 1px solid $color_grey;
+        border-bottom: 1px solid $color_grey;
+    }
+
+    a{
+        text-decoration: none;
+        color: $color_white;
+    }
+
+
+    .bottone{
+        background-color: $color_green;
+        padding: 1rem;
     }
 </style>
