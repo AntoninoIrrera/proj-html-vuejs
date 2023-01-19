d2c67cc456341855c595406bad65404e3adf99f8
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-19 15:01:26 +0100

    inzio inserimento contenuti quarta section del main

--- src/components/ComponentsMain/SectionJumbotron2.vue
+++ src/components/ComponentsMain/SectionJumbotron2.vue
@@ -6,20 +6,44 @@
 
 <template>
     <section>
-        <p class="m-0 text-white testo_centrale">prova</p>
+        <p class="m-0 text-white text-center testo_centrale">
+            <h2><span>COMING SOON MOVIE</span></h2>
+            <h1>MAX STILL</h1>  
+            <div class="riquadro">
+                <h2>JON PLAYER <span>&</span> EMILY ROSE</h2>
+                <h2>THE COUNTDOWN IS FINISHED!</h2>
+            </div>
+        </p>
     </section>
 </template>
 
 <style lang="scss" scoped>
+@use '../../styles/partials/variables' as *;
+
     section {
-        background-color: red;
+        background-image: url(../../assets/img/coming-bg.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: top;
         height: 75vh;
         position: relative;
     }
 
+    span{
+        color: $color_green;
+    }
+
+    h2{
+        font-size: 1.5rem;
+    }
+
+    .riquadro{
+        background-color: $color_main;
+        padding: 1rem;
+    }
     .testo_centrale {
         position: absolute;
-        left: 60%;
-        top: 50%;
+        left: 50%;
+        top: 30%;
     }
 </style>
