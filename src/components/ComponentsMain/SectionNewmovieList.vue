4d204b8f760d546103b97d134b99f3b879f0479e
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-19 21:13:24 +0100

    sistemato over quinta section del main

--- src/components/ComponentsMain/SectionNewmovieList.vue
+++ src/components/ComponentsMain/SectionNewmovieList.vue
@@ -5,10 +5,76 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 export default{
     data() {
         return {
-            contatoreDisplayMovieList: 0,
-        };
+            contatoreDisplayMovieList: -1,
+            movieList:[
+                {
+                    img:'1.jpg',
+                    durata: '2.30.00',
+                    star: '0/10',
+                    text: 'Hurry Animate Blue Strack New Movie',
+                    category: 'Top Rating',
+                    relese: 'February 12, 2016',
+                    genres: 'Drama, Action Releas',
+                    views: '381',
+                },
+                {
+                    img: '5-1.jpg',
+                    durata: '2.30.00',
+                    star: '0/10',
+                    text: 'Animate Blue Strack New Movie',
+                    category: 'Latest Movie',
+                    relese: 'February 12, 2016',
+                    genres: 'Drama, Action Releas',
+                    views: '343',
+                },
+                {
+                    img: '3.jpg',
+                    durata: '2.30.00',
+                    star: '0/10',
+                    text: 'Blue Strack New Animate Movie',
+                    category: 'Coming Soon',
+                    relese: 'February 12, 2016',
+                    genres: 'Drama, Action Releas',
+                    views: '420',
+                },
+                {
+                    img: '4.jpg',
+                    durata: '2.30.00',
+                    star: '0/10',
+                    text: 'Nulla Strack New Animate Movie',
+                    category: 'TV Series',
+                    relese: 'February 12, 2016',
+                    genres: 'Drama, Action Releas',
+                    views: '324',
+                },
+                {
+                    img: '3 (1).jpg',
+                    durata: '2.30.00',
+                    star: '1/10',
+                    text: 'Quisque In Strack New Movie',
+                    category: 'Top Rating',
+                    relese: 'February 12, 2016',
+                    genres: 'Drama, Action Releas',
+                    views: '339',
+                },
+                {
+                    img: '2.jpg',
+                    durata: '2.30.00',
+                    star: '0/10',
+                    text: 'New Movie Quisque In Strack',
+                    category: 'Latest Movie',
+                    relese: 'February 12, 2016',
+                    genres: 'Drama, Action Releas',
+                    views: '503',
+                },
+            ]
+        }
     },
-    components: { FontAwesomeIcon }
+    methods: {
+        getImagePath(imgPath) {
+            return new URL(`../../assets/img/${imgPath}`, import.meta.url).href;
+        }
+    }
 }
 
 
@@ -24,7 +90,33 @@ export default{
                         <p class="text-white m-0">Lorem Ipsum is simply dummy text of the printing and typesettin</p>
                     </p>
                 </div>
-                <div class="col-10 mt-5">
+                <div v-for="movie,index in movieList" class="col-4 mt-5" @mouseover="this.contatoreDisplayMovieList = index" @mouseleave="this.contatoreDisplayMovieList = -1">
+                    <img :src=getImagePath(movie.img) alt="2">
+                    <p class="text-white m-0 scritta-left" :class="this.contatoreDisplayMovieList == index ? `` : `d-none`">
+                       {{movie.durata}}
+                    </p>
+                    <p class="text-white m-0 scritta-right">
+                        <font-awesome-icon class="stella" icon="fa-solid fa-star" />
+                        {{movie.star}}
+                    </p>
+                    <font-awesome-icon :class="this.contatoreDisplayMovieList == index ? `` : `d-none`" class="play"
+                        icon="fa-regular fa-circle-play" />
+                    <h2 class="text-white m-0 scritta-up" :class="this.contatoreDisplayMovieList == index ? `scritta-up_piusu` : ``">
+                        {{movie.text}}
+                    </h2>
+                    <h2 class="text-white m-0 scritta-down" :class="this.contatoreDisplayMovieList == index ? `scritta-down_piusu` : ``">
+                        Category: {{movie.category}}
+                    </h2>
+                    <h2 class="text-white m-0 scritta-a-comparsa-up" :class="this.contatoreDisplayMovieList == index ? `` : `d-none`">
+                        Release: {{movie.relese}}
+                    </h2>
+                    <h2 class="text-white m-0 scritta-a-comparsa-down" :class="this.contatoreDisplayMovieList == index ? `` : `d-none`">
+                        Genres: {{movie.genres}}
+                    </h2>
+                    <a href="#" class="bottone-left">Details</a>
+                    <a href="#" class="bottone-right">{{movie.views}} Views</a>
+                </div>
+                <!-- <div class="col-10 mt-5">
                     <p class="text-white m-0">
                         <ul class="d-flex justify-content-start list-unstyled text-white">
                             <li class="ms-4">All</li>
@@ -62,79 +154,134 @@ export default{
                 </div>
                 <div class="col-4 mt-5">
                     <img src="../../assets/img/5-1.jpg" alt="5-1">
+                    <p class="text-white m-0 scritta-left" :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`">
+                        2.30.00
+                    </p>
                     <p class="text-white m-0 scritta-right">
                         <font-awesome-icon class="stella" icon="fa-solid fa-star" />
                         0/10
                     </p>
-                    <h2 class="text-white m-0 scritta-up">
-                        Animate Blue Strack New Movie
+                    <font-awesome-icon :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`" class="play"
+                        icon="fa-regular fa-circle-play" />
+                    <h2 class="text-white m-0 scritta-up" :class="this.contatoreDisplayMovieList == 1 ? `scritta-up_piusu` : ``">
+                        Hurry Animate Blue Strack New Movie
                     </h2>
-                    <h2 class="text-white m-0 scritta-down">
-                        Category: Latest Movie
+                    <h2 class="text-white m-0 scritta-down" :class="this.contatoreDisplayMovieList == 1 ? `scritta-down_piusu` : ``">
+                        Category: Top Rating
+                    </h2>
+                    <h2 class="text-white m-0 scritta-a-comparsa-up" :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`">
+                        Release: February 12, 2016
+                    </h2>
+                    <h2 class="text-white m-0 scritta-a-comparsa-down" :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`">
+                        Genres: Drama, Action Releas
                     </h2>
                     <a href="#" class="bottone-left">Details</a>
                     <a href="#" class="bottone-right">343 Views</a>
                 </div>
                 <div class="col-4 mt-5">
                     <img src="../../assets/img/3.jpg" alt="3">
+                    <p class="text-white m-0 scritta-left" :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`">
+                        2.30.00
+                    </p>
                     <p class="text-white m-0 scritta-right">
                         <font-awesome-icon class="stella" icon="fa-solid fa-star" />
                         0/10
                     </p>
-                    <h2 class="text-white m-0 scritta-up">
-                        Blue Strack New Animate Movie
+                    <font-awesome-icon :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`" class="play"
+                        icon="fa-regular fa-circle-play" />
+                    <h2 class="text-white m-0 scritta-up" :class="this.contatoreDisplayMovieList == 1 ? `scritta-up_piusu` : ``">
+                        Hurry Animate Blue Strack New Movie
                     </h2>
-                    <h2 class="text-white m-0 scritta-down">
-                        Category: Coming Soon
+                    <h2 class="text-white m-0 scritta-down" :class="this.contatoreDisplayMovieList == 1 ? `scritta-down_piusu` : ``">
+                        Category: Top Rating
+                    </h2>
+                    <h2 class="text-white m-0 scritta-a-comparsa-up" :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`">
+                        Release: February 12, 2016
+                    </h2>
+                    <h2 class="text-white m-0 scritta-a-comparsa-down" :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`">
+                        Genres: Drama, Action Releas
                     </h2>
                     <a href="#" class="bottone-left">Details</a>
                     <a href="#" class="bottone-right">420 Views</a>
                 </div>
                 <div class="col-4 mt-5">
                     <img src="../../assets/img/4.jpg" alt="4">
+                    <p class="text-white m-0 scritta-left" :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`">
+                        2.30.00
+                    </p>
                     <p class="text-white m-0 scritta-right">
                         <font-awesome-icon class="stella" icon="fa-solid fa-star" />
                         0/10
                     </p>
-                    <h2 class="text-white m-0 scritta-up">
-                        Nulla Strack New Animate Movie
+                    <font-awesome-icon :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`" class="play"
+                        icon="fa-regular fa-circle-play" />
+                    <h2 class="text-white m-0 scritta-up" :class="this.contatoreDisplayMovieList == 1 ? `scritta-up_piusu` : ``">
+                        Hurry Animate Blue Strack New Movie
                     </h2>
-                    <h2 class="text-white m-0 scritta-down">
-                        Category: TV Series
+                    <h2 class="text-white m-0 scritta-down" :class="this.contatoreDisplayMovieList == 1 ? `scritta-down_piusu` : ``">
+                        Category: Top Rating
+                    </h2>
+                    <h2 class="text-white m-0 scritta-a-comparsa-up" :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`">
+                        Release: February 12, 2016
+                    </h2>
+                    <h2 class="text-white m-0 scritta-a-comparsa-down" :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`">
+                        Genres: Drama, Action Releas
                     </h2>
                     <a href="#" class="bottone-left">Details</a>
                     <a href="#" class="bottone-right">324 Views</a>
                 </div>
                 <div class="col-4 mt-5">
                     <img src="../../assets/img/3 (1).jpg" alt="3">
+                    <p class="text-white m-0 scritta-left" :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`">
+                        2.30.00
+                    </p>
                     <p class="text-white m-0 scritta-right">
                         <font-awesome-icon class="stella" icon="fa-solid fa-star" />
                         1/10
                     </p>
-                    <h2 class="text-white m-0 scritta-up">
-                        Quisque In Strack New Movie
+                    <font-awesome-icon :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`" class="play"
+                        icon="fa-regular fa-circle-play" />
+                    <h2 class="text-white m-0 scritta-up" :class="this.contatoreDisplayMovieList == 1 ? `scritta-up_piusu` : ``">
+                        Hurry Animate Blue Strack New Movie
                     </h2>
-                    <h2 class="text-white m-0 scritta-down">
+                    <h2 class="text-white m-0 scritta-down" :class="this.contatoreDisplayMovieList == 1 ? `scritta-down_piusu` : ``">
                         Category: Top Rating
+                    </h2>
+                    <h2 class="text-white m-0 scritta-a-comparsa-up" :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`">
+                        Release: February 12, 2016
+                    </h2>
+                    <h2 class="text-white m-0 scritta-a-comparsa-down" :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`">
+                        Genres: Drama, Action Releas
                     </h2>
                     <a href="#" class="bottone-left">Details</a>
                     <a href="#" class="bottone-right">339 Views</a>
                 </div>
                 <div class="col-4 mt-5">
                     <img src="../../assets/img/2.jpg" alt="2">
+                    <p class="text-white m-0 scritta-left" :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`">
+                        2.30.00
+                    </p>
                     <p class="text-white m-0 scritta-right">
                         <font-awesome-icon class="stella" icon="fa-solid fa-star" />
                         0/10
                     </p>
-                    <h2 class="text-white m-0 scritta-up">
-                        New Movie Quisque In Strack
+                    <font-awesome-icon :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`" class="play"
+                        icon="fa-regular fa-circle-play" />
+                    <h2 class="text-white m-0 scritta-up" :class="this.contatoreDisplayMovieList == 1 ? `scritta-up_piusu` : ``">
+                        Hurry Animate Blue Strack New Movie
                     </h2>
-                    <h2 class="text-white m-0 scritta-down">
-                        Category: Latest Movie
+                    <h2 class="text-white m-0 scritta-down" :class="this.contatoreDisplayMovieList == 1 ? `scritta-down_piusu` : ``">
+                        Category: Top Rating
+                    </h2>
+                    <h2 class="text-white m-0 scritta-a-comparsa-up" :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`">
+                        Release: February 12, 2016
+                    </h2>
+                    <h2 class="text-white m-0 scritta-a-comparsa-down" :class="this.contatoreDisplayMovieList == 1 ? `` : `d-none`">
+                        Genres: Drama, Action Releas
                     </h2>
                     <a href="#" class="bottone-left">Details</a>
                     <a href="#" class="bottone-right">503 Views</a>
-                </div>
+                </div> -->
             </div>
         </div>
     </section>
